2e1a025a5407cb3f0d685c8a9f57a3211255b112
Kyle Gehmlich <kgehmlich@gmail.com>
2020-06-14 21:44:33 -0400

    Pack with version from release tag

--- get-version.sh
+++ get-version.sh
@@ -4,11 +4,14 @@ version=${BUILD_SOURCEBRANCH#*v}
 echo "Version:" $version
 
 major=${version%%.*}
+echo "##vso[task.setvariable variable=MajorVersion]$major"
 echo "Major:" $major
 
 minor=${version%.*}
 minor=${minor#*.}
+echo "##vso[task.setvariable variable=MinorVersion]$minor"
 echo "Minor:" $minor
 
 patch=${version##*.}
+echo "##vso[task.setvariable variable=PatchVersion]$patch"
 echo "Patch:" $patch
