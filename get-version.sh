a2058ac69a7e39d592cc3042c00931c3968c89cb
Kyle Gehmlich <kgehmlich@gmail.com>
2020-06-15 13:28:24 -0400

    Parameterize get-version script

--- get-version.sh
+++ get-version.sh
@@ -1,17 +1,24 @@
 #! /bin/bash
 
-version=${BUILD_SOURCEBRANCH#*v}
+# Takes a single argument: the release tag prefix (i.e. the part
+# that, once stripped off, leaves behind the version number)
+# E.g. if release tags are `release/v1.2.3` then the prefix is `release/v`
+
+if [ "$#" -ne 1 ]; then
+    echo "ERROR: Incorrect number of arguments."
+    echo "Usage: get-version release/prefix"
+    exit 1
+fi
+
+version=${BUILD_SOURCEBRANCH#*$1}
 echo "Version:" $version
 
 major=${version%%.*}
 echo "##vso[task.setvariable variable=MajorVersion]$major"
-echo "Major:" $major
 
 minor=${version%.*}
 minor=${minor#*.}
 echo "##vso[task.setvariable variable=MinorVersion]$minor"
-echo "Minor:" $minor
 
 patch=${version##*.}
 echo "##vso[task.setvariable variable=PatchVersion]$patch"
-echo "Patch:" $patch
